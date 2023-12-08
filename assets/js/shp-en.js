ab8f131e6a3c2328d875683d26b12c7adf6279eb
estds <56233807+estds@users.noreply.github.com>
2023-12-08 17:00:47 +0800

    Update shp-en.js

--- assets/js/shp-en.js
+++ assets/js/shp-en.js
@@ -1,6 +1,6 @@
 var assetsURL = 'https://fastly.jsdelivr.net/gh/estds/gef-china-shp-cap-website-small-and-green/assets';
 
-const jsonURL = assetsURL + '/data/all-content-v12.1.json';
+const jsonURL = assetsURL + '/data/all-content-v12.1-en.json';
 
 let translation = {
   "caseStudy": "Case study",
